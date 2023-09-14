234d81cdb869e2d09849bbe3b5d9f6f9b9a3250f
FlaviusMiron <flaviusmiron47@gmail.com>
2023-09-14 15:16:18 +0300

    Update train_model.py
    added soft-max regression

--- train_model.py
+++ train_model.py
@@ -1,15 +1,18 @@
 """
-Script used to train the neural network. Showcases 2 model variants that can achieve 100%
+Script used to train the neural network and the softmax model. Showcases multiple model variants that can achieve 100%
 accuracy on the unseen test_data (given some luck on the starting parameters :) )
-Should be run with "Run current file in interactive window" to see the plots of the cost on training and test data.
 """
 import data_loader
 import neural_network
+import soft_max
 
 training_data, test_data = data_loader.get_data(0.8)
 
-model_nn = neural_network.MLP([4,12,3])
-model_nn.SGD(training_data, test_data, mini_batch_size = 20, epochs = 1000, learning_rate = 0.05)
+# model_nn = neural_network.MLP([4,12,3])
+#model_nn.SGD(training_data, test_data, mini_batch_size = 20, epochs = 1000, learning_rate = 0.05)
 
-# model_nn = neural_network.MLP([4,30,3])
-# model_nn.SGD(training_data, test_data, mini_batch_size = 5, epochs = 100, learning_rate = 0.1)
+model_nn = neural_network.MLP([4,30,3])
+model_nn.SGD(training_data, test_data, mini_batch_size = 5, epochs = 100, learning_rate = 0.1)
+
+model_sm = soft_max.soft_max_logistic_regression(3,4)
+model_sm.SGD(training_data, test_data)
